2cf2f260074eb4679fd3d72302851e8131d700d8
Enzo-orbe <enzoforbe@gmail.com>
2021-01-27 16:11:56 -0300

    Home Styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../store/actions/LoginActions";
 
@@ -20,9 +20,155 @@ export default function Home() {
     history.push("/");
   };
   return (
-    <div>
-      <h1>Bienvenido {users.user.name}</h1>
-      <button onClick={handleClick}>logout</button>
+    <div className=" w-100 md:w-50 lg:w-50 px-10 mt-5  mx-15   items-center justify-center">
+      <div className="relative bg-white overflow-hidden">
+        <div className="max-w-7xl mx-0-auto">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <svg
+              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+              fill="currentColor"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="50,0 100,0 50,100 0,100" />
+            </svg>
+
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <nav
+                className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                aria-label="Global"
+              >
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <Link to="/home">
+                      <span className="sr-only">Workflow</span>
+                      <img
+                        className="h-25 w-25 sm:h-20"
+                        src="https://cdn.pixabay.com/photo/2017/01/13/01/22/rocket-1976107_960_720.png"
+                        alt="test"
+                      />
+                    </Link>
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <button
+                        type="button"
+                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                        id="main-menu"
+                        aria-haspopup="true"
+                      >
+                        <span className="sr-only">Open main menu</span>
+
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                  <button
+                    onClick={handleClick}
+                    className="font-medium text-gray-600 hover:text-gray-500"
+                  >
+                    Log out
+                  </button>
+                </div>
+              </nav>
+            </div>
+            <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="px-5 pt-4 flex items-center justify-between">
+                  <div>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <button
+                      type="button"
+                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                    >
+                      <span className="sr-only">Close main menu</span>
+
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="main-menu"
+                >
+                  <div role="none">
+                    <button
+                      onClick={handleClick}
+                      className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                      role="menuitem"
+                    >
+                      Log out
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">
+                    Bienvenido {users.user.name}
+                  </span>
+                </h1>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <div className="mt-3 sm:mt-0 sm:ml-3">
+                    <button
+                      onClick={handleClick}
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 md:py-4 md:text-lg md:px-10"
+                    >
+                      Log Out
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <img
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src="https://cdn.pixabay.com/photo/2015/11/09/14/43/laptop-1035345_960_720.jpg"
+            alt="test"
+          />
+        </div>
+      </div>
     </div>
   );
 }
